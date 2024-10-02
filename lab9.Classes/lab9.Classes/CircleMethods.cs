71dbe87c47dbd801e8e7f6f698a2dd43fe064f8c
DeDMaKSiM1 <152630416+DeDMaKSiM1@users.noreply.github.com>
2024-10-02 21:17:25 +0800

    lab9

---
+++ lab9.Classes/lab9.Classes/CircleMethods.cs
@@ -0,0 +1,60 @@
+﻿using System;
+
+public class CircleMethods
+{
+    private int _gradus, _min, _sec;
+
+    public int Gradus
+    {
+        get => _gradus;
+        set
+        {
+            if (value >= 0)
+                _gradus = value;
+        }
+    }
+    public int Minute
+    {
+        get => _min;
+        set
+        {
+            if (value >= 60)
+            {
+                Gradus += value / 60;
+                _min = value % 60;
+            }
+
+            else if (value >= 0)
+                _min = value;
+        }
+    }
+    public int Second
+    {
+        get => _sec;
+        set
+        {
+            if (value >= 60)
+            {
+                Minute += value / 60;
+                _sec = value % 60;
+            }
+            else if (value >= 0)
+                _sec = value;
+
+        }
+    }
+    public CircleMethods(int gradus, int min, int sec)
+    {
+        Gradus = gradus;
+        Minute = min;
+        Second = sec;
+    }
+
+
+    public double ToRadians()
+    {
+        double result = Gradus + (Minute + Second / 60.0) / 60.0;
+        return result * Math.PI / 180.0;
+    }
+}
+
---
+++ lab9.Classes/lab9.Classes/EntryPoint.cs
@@ -0,0 +1,46 @@
+﻿using System;
+
+public class EntryPoint
+{
+    static void Main()
+    {
+        /*
+        Угол задан с помощью целочисленных значений gradus - градусов, min - угловых минут, sec - угловых секунд. 
+        Реализовать класс, в котором указанные значения представлены в виде свойств. 
+        Для свойств предусмотреть проверку корректности данных. 
+        Класс должен содержать конструктор для установки начальных значений, 
+        а также метод ToRadians для перевода угла в радианы. Создать объект на основе разработанного класса. 
+        Осуществить использование объекта в программе.
+        */
+        int gradus, minutes, seconds;
+        try
+        {
+            Console.WriteLine("Введите значение градуса");
+            if (!int.TryParse(Console.ReadLine(), out gradus))
+                throw new ArgumentException("Ошибка в формате значения градуса");
+
+            Console.WriteLine("Введите значение минут");
+            if (!int.TryParse(Console.ReadLine(), out minutes))
+                throw new ArgumentException("Ошибка в формате значения минут");
+
+            Console.WriteLine("Введите значение секунд");
+            if (!int.TryParse(Console.ReadLine(), out seconds))
+                throw new ArgumentException("Ошибка в формате значения секунд");
+
+        }
+        catch (ArgumentException arEx)
+        {
+            Console.WriteLine(arEx.Message);
+            return;
+        }
+        var circle1 = new CircleMethods(gradus, minutes, seconds);
+
+        Console.WriteLine($"Градусов {circle1.Gradus}");
+        Console.WriteLine($"Минут {circle1.Minute}");
+        Console.WriteLine($"Секунды {circle1.Second}");
+
+
+        Console.WriteLine($"После перевода в радианы: {circle1.ToRadians():0.###}");
+    }
+}
+
