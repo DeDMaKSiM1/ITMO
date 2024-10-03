14aa31e42022d0c8bd4aeff802296179ebe8407a
DeDMaKSiM1 <152630416+DeDMaKSiM1@users.noreply.github.com>
2024-10-03 16:30:03 +0800

    lab11

---
+++ lab11/lab11/Circle.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+public class Circle
+{
+    public static double GetPerimeter(double radius)
+    {
+        return 2.0 * Math.PI * radius;
+    }
+    public static double GetArea(double radius)
+    {
+        return Math.PI * radius * radius;
+    }
+    public static bool IsInCircle(double radius, params double[] coordinateValues)
+    {
+
+        double xC = coordinateValues[0];
+        double yC = coordinateValues[1];
+        double x1 = coordinateValues[2];
+        double y1 = coordinateValues[3];
+
+        if (Math.Pow(x1 - xC, 2) + Math.Pow(y1 - yC, 2) <= Math.Pow(radius, 2))
+            return true;
+        return false;
+    }
+
+}
+
+
+
---
+++ lab11/lab11/EntryPoint.cs
@@ -0,0 +1,54 @@
+﻿using System;
+
+public class EntryPoint
+{
+    static void Main()
+    {
+        double[] coordinateArrValues = new double[4];
+        char[] delimiters = { ' ' };
+        double radius;
+        Console.WriteLine("Введите радиус окружности");
+
+        try
+        {
+            if (!double.TryParse(Console.ReadLine(), out radius))
+                throw new ArgumentException("Ошибка в формате значения радиуса");
+            if (radius <= 0)
+                throw new ArgumentException("Радиус меньше либо равен нулю");
+
+            Console.WriteLine("Введите координаты центра окружности xC, yC и координаты точки x1,y2 через пробел");
+            string[] coordnateArr = Console.ReadLine().Split(delimiters, StringSplitOptions.RemoveEmptyEntries);
+
+            if (coordnateArr.Length != 4)
+                throw new ArgumentOutOfRangeException("Передано меньше или больше четырех значений координат");
+            for (int i = 0; i < coordnateArr.Length; i++)
+            {
+                if (!double.TryParse(coordnateArr[i], out double value))
+                    throw new ArgumentException("Ошибка в формате данных координат");
+                coordinateArrValues[i] = value;
+            }
+
+
+            Console.WriteLine($"Периметр равен {Circle.GetPerimeter(radius):0.##}");
+            Console.WriteLine($"Площадь равна {Circle.GetArea(radius):0.##}");
+            if (Circle.IsInCircle(radius, coordinateArrValues))
+                Console.WriteLine("Точка лежит внутри окружности");
+            else
+                Console.WriteLine("Точка лежит вне окружности");
+
+        }
+        catch (ArgumentException ex)
+        {
+            Console.WriteLine(ex.Message);
+            return;
+        }
+        catch
+        {
+            Console.WriteLine("Неизвестная ошибка");
+            return;
+        }
+
+
+    }
+}
+
