bee5634ca51ce649df4d0190efa3d4189ec7e19e
DeDMaKSiM1 <152630416+DeDMaKSiM1@users.noreply.github.com>
2024-10-03 14:53:29 +0800

    lab10

---
+++ lab10/Struct/lab10/EntryPoint.cs
@@ -0,0 +1,38 @@
+﻿using System;
+
+public class EntryPoint
+{
+    static void Main()
+    {
+        /*
+        Разработать структуру для решения линейного уравнения 0=kx+b. 
+        Коэффициенты уравнения k, b реализовать с помощью полей вещественного типа. 
+        Для решения уравнения предусмотреть метод Root. 
+        Создать экземпляр разработанной структуры. 
+        Осуществить использование экземпляра в программе.
+        */
+        double k, b;
+        Console.WriteLine("Введите k и b для уравнения вида kx + b = 0 через пробелы");
+        char[] delimiters = { ' ' };
+        string[] inputLine = Console.ReadLine().Split(delimiters, StringSplitOptions.RemoveEmptyEntries);
+        try
+        {
+            if (inputLine.Length != 2)
+                throw new ArgumentOutOfRangeException("Введено больше или меньше 2 значений");
+            if (!double.TryParse(inputLine[0], out k))
+                throw new ArgumentException("Ошибка формата k");
+            if (!double.TryParse(inputLine[1], out b))
+                throw new ArgumentException("Ошибка формата b");
+        }
+        catch(ArgumentException arEx)
+        {
+            Console.WriteLine(arEx.Message);
+            return;
+        }
+        var linearEq = new LinearEquation(k, b);
+
+        Console.WriteLine($"{linearEq.Root():0:##}");
+        Console.ReadKey();
+    }
+}
+
---
+++ lab10/Struct/lab10/LinearEquation.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+
+public struct LinearEquation
+{
+    public double K { get; private set; }
+    public double B { get; private set; }
+
+    public LinearEquation(double K, double B)
+    {
+        this.K = K;
+        this.B = B;
+    }
+
+    public string Root()
+    {
+        if (K == 0 && B != 0)
+            return "Уравнение корней не имеет";
+        if (K == 0 && B == 0)
+            return "Корнем уравнения является любое число";
+        double result;
+        result = -B / K;
+        return $"Корень уравнения {result}";
+    }
+}
+
