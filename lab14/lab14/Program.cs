bc21d6928d643c736295a2cf9132fbf88baa751a
DeDMaKSiM1 <152630416+DeDMaKSiM1@users.noreply.github.com>
2024-10-07 21:54:29 +0800

    lab14

---
+++ lab14/lab14/Program.cs
@@ -0,0 +1,47 @@
+﻿using System;
+
+
+public class Program
+{
+    static void Main()
+    {
+        var arithm = new ArithProgression();
+        var geometric = new GeomProgression();
+        try
+        {
+            Console.WriteLine("Арифметическая прогрессия");
+            arithm.SetStart(5);
+            arithm.SetStep(5);
+            for (int i = 0; i < 5; i++)
+            {
+                Console.WriteLine(arithm.GetNext());
+            }
+            arithm.Reset();
+            Console.WriteLine(arithm.CurrentValue);
+
+            Console.WriteLine("\nГеометрическая прогрессия");
+            geometric.SetStep(2);
+            geometric.SetStart(1);
+            for (int i = 0; i < 5; i++)
+            {
+                Console.WriteLine(geometric.GetNext());
+            }
+            geometric.Reset();
+            Console.WriteLine(geometric.CurrentValue);
+
+        }
+        catch(ArgumentException arEx)
+        {
+            Console.WriteLine(arEx.Message);
+            return;
+        }
+        catch
+        {
+            Console.WriteLine("Неизвестная ошибка");
+            return;
+        }
+
+
+    }
+}
+
---
+++ lab14/lab14/Progressions.cs
@@ -0,0 +1,62 @@
+﻿using System;
+
+
+public interface ISeries
+{
+    void SetStart(int x);
+    int GetNext();
+    void Reset();
+}
+
+public class ArithProgression : ISeries
+{
+    public int Step { get; private set; }
+    public int StartValue { get; private set; }
+    public int CurrentValue { get; private set; }
+
+    public int GetNext()
+    {
+        if (Step == 0 || StartValue == 0)
+            throw new ArgumentException("Начальное число либо шаг прогрессии равен нулю");
+        return CurrentValue += Step;
+    }
+    public void Reset()
+    {
+        CurrentValue = StartValue;
+    }
+    public void SetStart(int startNum)
+    {
+        StartValue = startNum;
+        CurrentValue = startNum;
+    }
+    public void SetStep(int step)
+    {
+        Step = step;
+    }
+}
+public class GeomProgression : ISeries
+{
+    public int Koef { get; private set; }
+    public int StartValue { get; private set; }
+    public int CurrentValue { get; private set; }
+
+    public int GetNext()
+    {
+        if (Koef == 0 || StartValue == 0)
+            throw new ArgumentException("Начальное число либо коэффициент прогрессии равен нулю");
+        return CurrentValue *= Koef;
+    }
+    public void Reset()
+    {
+        CurrentValue = StartValue;
+    }
+    public void SetStart(int startNum)
+    {
+        StartValue = startNum;
+        CurrentValue = startNum;
+    }
+    public void SetStep(int koef)
+    {
+        Koef = koef;
+    }
+}
