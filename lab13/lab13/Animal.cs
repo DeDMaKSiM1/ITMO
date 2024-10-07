447693ac759cefb082e159b4f83759b5bd718acb
DeDMaKSiM1 <152630416+DeDMaKSiM1@users.noreply.github.com>
2024-10-07 14:02:48 +0800

    lab13. Abstract classes

---
+++ lab13/lab13/Animal.cs
@@ -0,0 +1,36 @@
+﻿using System;
+public abstract class Animal
+{
+    public abstract string Name { get;  protected set; }
+    
+    public Animal(string name)
+    {
+        Name = name;
+    }
+    public Animal() : this("Животное")
+    {
+    }
+    public void ShowInfo()
+    {
+        Console.Write($"{Name} ");
+        Say();
+    }
+    public abstract void Say();
+}
+public class Cat : Animal
+{
+    public override string Name { get; protected set; }
+    public Cat() : base("Кошка")
+    {
+    }
+    public override void Say() => Console.WriteLine("Мяу");
+}
+public class Dog : Animal
+{
+    public override string Name { get; protected set; }
+    public Dog() : base("Собака")
+    {
+    }
+    public override void Say() => Console.WriteLine("Гав");
+}
+
---
+++ lab13/lab13/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+public class Program
+{
+    static void Main()
+    {
+        var dog = new Dog();
+        dog.ShowInfo();
+        dog.Say();
+
+        var cat = new Cat();
+        cat.ShowInfo();
+        cat.Say();
+    }
+}
+
