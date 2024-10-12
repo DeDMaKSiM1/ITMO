e7017ec042f50af8efc10f9e208b8cf1856e1b74
DeDMaKSiM1 <152630416+DeDMaKSiM1@users.noreply.github.com>
2024-10-12 17:39:59 +0800

    lab17.Stack

---
+++ lab17.Stack/lab17.Stack/Program.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+public class Program
+{
+    static void Main()
+    {
+        string inputLine = "{[]}";
+        Stack<char> bracketsChars = new Stack<char>();
+        bool isValidLine = true;
+
+        for (int i = 0; i < inputLine.Length; i++)
+        {
+            if (!isValidLine)
+                break;
+            switch (inputLine[i])
+            {
+                case '(':
+                    bracketsChars.Push(inputLine[i]);
+                    bracketsChars.Push((char)((int)inputLine[i] + 1));
+                    break;
+                case '[':
+                case '{':
+                    bracketsChars.Push(inputLine[i]);
+                    bracketsChars.Push((char)((int)inputLine[i] + 2));
+                    break;
+
+                case '}':
+                case ')':
+                case ']':
+                    if (bracketsChars.Count == 0 || bracketsChars.Peek() != inputLine[i])
+                    {
+                        isValidLine = false;
+                        break;
+                    }
+                    bracketsChars.Pop();
+                    bracketsChars.Pop();
+                    break;
+
+                default:
+                    isValidLine = false;
+                    break;
+            }
+        }
+
+        if (!isValidLine)
+        {
+            Console.WriteLine("Скобки расставлены некорректно");
+            return;
+        }
+        Console.WriteLine("Скобки расставлены корректно");
+    }
+
+
+}
+
